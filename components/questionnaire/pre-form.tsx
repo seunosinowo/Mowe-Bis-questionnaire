082e53c1195dd18fe4e7c9e8b29ca854833c80d9
seunosinowo <oluwaseunpaul98@gmail.com>
2026-02-27 15:25:10 +0100

    update details

--- components/questionnaire/pre-form.tsx
+++ components/questionnaire/pre-form.tsx
@@ -95,7 +95,6 @@ export default function PreForm({ onSubmit }: PreFormProps) {
                 <SelectContent>
                   <SelectItem value="male">Male</SelectItem>
                   <SelectItem value="female">Female</SelectItem>
-                  <SelectItem value="non-binary">Non-binary</SelectItem>
                   <SelectItem value="prefer-not-to-say">Prefer not to say</SelectItem>
                 </SelectContent>
               </Select>
